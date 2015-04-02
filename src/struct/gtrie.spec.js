7706928084143739aa59ed680f4c6d3d13620cdb
andreas <andreastausend@gmx.de>
2015-04-02 17:36:03 +0200

    added prediction method, including a 'no prediction'- catch

--- src/struct/gtrie.spec.js
+++ src/struct/gtrie.spec.js
@@ -27,6 +27,34 @@ import {GTrie} from 'gtrie';
         var seq = testTrie.sequence().input('one').input('two').input('three');
         expect(seq.allInputs()).to.have.length(3);
       });
+
+      it('Sequences predict next input', () => {
+        var testTrie = new GTrie();
+        var seq = testTrie.sequence().input('one').input('two').input('three');
+        var seq2 = testTrie.sequence().input('one');
+        //expect(seq2.prediction().getprediction(0).prediction).not.to.be.null;
+        expect(seq2.prediction().getprediction(1).prediction).to.equal('no prediction');
+        expect(seq2.prediction().getprediction(0).prediction).to.equal('two');
+        //seq2.input('two');
+        //expect(seq2.prediction().get(0)[0]).to.equal('three');
+      });
+      it('Prediction matches expectation', () => {
+        var testTrie = new GTrie();
+        var seq = testTrie.sequence().input('one').input('two');
+        var seq2 = testTrie.sequence().input('one').input('two');
+        var seq3 = testTrie.sequence().input('one').input('two');
+        var seq4 = testTrie.sequence().input('one').input('two');
+        var seq5 = testTrie.sequence().input('one').input('three');
+        var seq6 = testTrie.sequence().input('one');;
+        expect(seq6.prediction().getprediction(0).prediction).to.equal('two');
+        expect(seq6.prediction().getprediction(0).relativeFrequency).to.equal(2/3);
+      });
+      it('Prediction matches expectation', () => {
+        var testTrie = new GTrie();
+        var seq = testTrie.sequence().input('one');
+        expect(seq.prediction().getprediction(0).prediction).to.equal('no prediction');
+      });
+
     });
   });
 })();
